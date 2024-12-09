59665918c67cd39403db065131e2b12d75f585b3
KS2013 <ksarker2013@gmail.com>
2024-12-09 21:58:56 +0000

    Add files via upload

---
+++ A1.py
@@ -0,0 +1,10 @@
+#Input the values of terms
+n = int(input("Enter the value of the terms: "))
+
+sum = 0 #initialise
+i = 1 #initialise
+while i<=n: #loop will run fom 1 to n
+    sum = sum + i
+    i = i+1
+
+print("\nSum =" , sum)
